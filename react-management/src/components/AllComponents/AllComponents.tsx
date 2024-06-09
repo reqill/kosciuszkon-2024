f3fddb793054baa5e76ad3262ddd9c1e94f63bfe
Łukasz Marcinkiewicz <marcinkiewi_cz@poczta.fm>
2024-06-09 11:25:44 +0200

    My commit

--- react-management/src/components/AllComponents/AllComponents.tsx
+++ react-management/src/components/AllComponents/AllComponents.tsx
@@ -5,6 +5,7 @@ import { WeatherBar } from "../WeatherBar/WeatherBar";
 
 export const AllComponents = () => {
   return (
+    // Zmiana
     <div className="flex flex-col space-y-2">
       <DateTimeBar />
       <div className="flex flex-row gap-2">
