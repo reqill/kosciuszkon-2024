bf73dbab7218c8dd8e75404c02d0164974f0418a
Mikołaj Mrózek <m.mik311@gmail.com>
2024-06-09 09:58:55 +0200

    feat: widget one

--- react-management/src/components/NameDisplayWidget/NameDisplayWidget.tsx
+++ react-management/src/components/NameDisplayWidget/NameDisplayWidget.tsx
@@ -1,14 +1,15 @@
 import { useSearchParams } from "react-router-dom";
+import { WidgetShapeWrapper } from "../WidgetShapeWrapper/WidgetShapeWrapper";
 
 export const NameDisplayWidget = () => {
   const [searchParams] = useSearchParams();
   const name = searchParams.get("name");
 
   return (
-    <div className="w-full p-1">
-      <h1 className="w-full text-center text-zinc-900 ring-inset ring-2 ring-zinc-900 font-semibold text-xl  py-2 rounded-md">
+    <WidgetShapeWrapper>
+      <h1 className="w-full text-center text-zinc-900 font-semibold text-xl py-2">
         {name}
       </h1>
-    </div>
+    </WidgetShapeWrapper>
   );
 };
