49614c1f8d2bfe6cdca69bb4dc4ab9a83c8cd571
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2024-06-09 10:56:04 +0200

    photo_wiget

--- react-management/src/components/PhotoWidget/PhotoWidget.tsx
+++ react-management/src/components/PhotoWidget/PhotoWidget.tsx
@@ -3,9 +3,14 @@ import { useSearchParams } from "react-router-dom";
 export const PhotoWidget = () => {
   const [searchParams] = useSearchParams();
   const photo_id = parseInt(searchParams.get("photo_id") ?? "0");
-  const photo_urls = ["https://ih1.redbubble.net/image.3317721448.0910/st,small,507x507-pad,600x600,f8f8f8.u1.jpg"];
+  const photo_urls = [
+    "https://firebasestorage.googleapis.com/v0/b/kosciuszkon-949d3.appspot.com/o/1_1.png?alt=media&token=ffb596c7-4797-4a72-99e5-edf5e72ba157",
+    "https://firebasestorage.googleapis.com/v0/b/kosciuszkon-949d3.appspot.com/o/1_2.png?alt=media&token=1aaa6be6-fcfa-41cd-9a3f-00ec1fd19bdd",
+    "https://firebasestorage.googleapis.com/v0/b/kosciuszkon-949d3.appspot.com/o/2_1.png?alt=media&token=2ac28df2-032d-428d-a2a5-52a8587b3f68",
+    "https://firebasestorage.googleapis.com/v0/b/kosciuszkon-949d3.appspot.com/o/2_2.png?alt=media&token=1a547f3e-f477-431c-9417-05ecdd996c50",
+  ];
   return (
-    <div className="w-full p-1">
+    <div className="w-full p-1 h-full overflow-hidden">
       <img src={photo_urls[photo_id]} width="100%" height="100%" style={{ objectFit: "contain" }} />
     </div>
   );
